98e2e7fbb2fffba5bc449d8dc3b35b7a657564cb
Likhith H L <likhith1022@gmail.com>
2026-07-07 21:43:53 +0530

    Fix formatting in return statement of scan results

--- gui.py
+++ gui.py
@@ -74,7 +74,9 @@ def load_stats():
             
     return {
         "total_scans": total_scans,
-        
+        "threats_detected": threats_detected,
+        "files_quarantined": files_quarantined,
+        "integrity_alerts": integrity_alerts
     }
 
 
